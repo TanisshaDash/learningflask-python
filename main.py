936810ba6f1356bd7a8495ffefa9e94b1c1d9afd
TanisshaDash <tanissha.1610@gmail.com>
2024-11-07 20:54:26 +0530

    Commits

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask  
 app = Flask(__name__)  
- 
-@app.route('/')  
-def home():  
-    return "hello, welcome to our website";  
+  
+def about():  
+    return "SODI page";  
+  
+app.add_url_rule("/about","about",about)  
   
 if __name__ =="__main__":  
     app.run(debug = True)  
