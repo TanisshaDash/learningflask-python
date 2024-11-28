958df94d72d8963b8412b6db4b82e501a9feeb63
TanisshaDash <tanissha.1610@gmail.com>
2024-11-28 12:23:00 +0530

    session updates

--- login.py
+++ login.py
@@ -1,33 +1,36 @@
 from flask import *  
-  
 app = Flask(__name__)  
- 
-@app.route('/error')  
-def error():  
-    return "<p><strong>Enter correct password</strong></p>"  
+app.secret_key = "tanissha"  
  
 @app.route('/')  
+def home():  
+    return render_template("home.html")  
+ 
+@app.route('/login')  
 def login():  
     return render_template("login.html")  
  
-@app.route('/success',methods = ['POST'])  
+@app.route('/success',methods = ["POST"])  
 def success():  
     if request.method == "POST":  
-        email = request.form['email']  
-        password = request.form['pass']  
-      
-    if password=="pewpew":  
-        resp = make_response(render_template('success.html'))  
-        resp.set_cookie('email',email)  
-        return resp  
-    else:  
-        return redirect(url_for('error'))  
+        session['email']=request.form['email']  
+    return render_template('success.html')  
  
-@app.route('/viewprofile')  
+@app.route('/logout')  
+def logout():  
+    if 'email' in session:  
+        session.pop('email',None)  
+        return render_template('logout.html');  
+    else:  
+        return '<p>user already logged out</p>'  
+ 
+@app.route('/profile')  
 def profile():  
-    email = request.cookies.get('email')  
-    resp = make_response(render_template('profile.html',name = email))  
-    return resp  
+    if 'email' in session:  
+        email = session['email']  
+        return render_template('profile.html',name=email)  
+    else:  
+        return '<p>Please login first</p>'  
   
-if __name__ == "__main__":  
+if __name__ == '__main__':  
     app.run(debug = True)  
