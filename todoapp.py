c91e7a00f482a68a6101fd99df25f81788dae8a8
TanisshaDash <tanissha.1610@gmail.com>
2025-01-21 00:45:53 +0530

    TodoListMini Project

--- todoapp.py
+++ todoapp.py
@@ -2,7 +2,7 @@ from flask import Flask , render_template,request, redirect,url_for
 
 app = Flask (__name__,template_folder="templates")
 
-todos = [{"task": "Sample Todo", "done": False}]
+todos = [{"task":"Sample Todo", "done": False}]
 
 @app.route("/")
 def index():
@@ -14,7 +14,6 @@ def add():
     todos.append({"task": todo, "done": False})
     return redirect(url_for("index"))
 
-
 @app.route("/edit/<int:index>", methods = ["GET", "POST"])
 def edit(index):
     todo = todos[index]
@@ -24,21 +23,15 @@ def edit(index):
     else:
         return render_template ("edit.html", todo = todo , index = index )
     
-
-
 @app.route("/check/<int:index>")
 def check(index):
     todos[index]['done'] = not todos [index]['done']
     return redirect(url_for("index"))
 
-
-
 @app.route("/delete/<int:index>")
 def delete(index):
     del todos[index]
     return redirect(url_for("index"))
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
