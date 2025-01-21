52f2f6189007967ec02015a2aee0cccfd7b6d28a
TanisshaDash <tanissha.1610@gmail.com>
2025-01-21 14:29:03 +0530

    Example Project forms

--- introFlask/app.py
+++ introFlask/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, flash, url_for, redirect, render_template  
-from flask_sqlalchemy import SQLAlchemy  
-  
+from flask_sqlalchemy import SQLAlchemy   
 app = Flask(__name__)  
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.sqlite3'  
 app.config['SECRET_KEY'] = "secret key"  
